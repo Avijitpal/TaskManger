ecc5f7447134d06291ca46497a9bce3656e4098c
Avijit <avijitpal4343@gmail.com>
2025-05-30 23:45:59 +0530

    Thera are some error

--- backEnd/server.js
+++ backEnd/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res) => {
+app.get('/Hello', (req, res) => {
   res.send('API is running...');
 });
 
